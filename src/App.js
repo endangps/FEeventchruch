e98355eee6ffaa2fc13ea30bced62d983076fd88
endangps <endanglps17@gmail.com>
2024-08-08 15:57:57 +0700

    Yang baru diupdate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="schedule">
+        <div className="schedule-header">
+          <h1>JADWAL AGUSTUS 2024</h1>
+          <h2>10 AGUSTUS 2024</h2>
+        </div>
+        <div className="schedule-body">
+          <div className="schedule-item">
+            <div className="schedule-title">
+              <h3>YOUTH QUAKE WITH JEREMY MANTOFA</h3>
+              <span className="location">AOG 1</span>
+            </div>
+            <div className="event-details">
+              <p className="mainhall">MAINHALL</p>
+              <div className="right-details">
+                <p className="floor">3A<sup>FL</sup></p>
+                <p className="time">15.00 - 16.45 <span className="wib">WIB</span></p>
+              </div>
+            </div>
+          </div>
+          <div className="schedule-item">
+            <div className="schedule-title">
+              <h3>YOUTH QUAKE WITH JEREMY MANTOFA</h3>
+              <span className="location">AOG 2</span>
+            </div>
+            <div className="event-details">
+              <p className="mainhall">MAINHALL</p>
+              <div className="right-details">
+                <p className="floor">3A<sup>FL</sup></p>
+                <p className="time">17.00 - 18.45 <span className="wib">WIB</span></p>
+              </div>
+            </div>
+          </div>
+          <div className="schedule-item">
+            <div className="schedule-title">
+              <h3>SPECIAL SERVICE WITH PS. PHILIP MANTOFA</h3>
+              <span className="location">UMUM 1</span>
+            </div>
+            <div className="event-details">
+              <p className="mainhall">MAINHALL</p>
+              <div className="right-details">
+                <p className="floor">3<sup>FL</sup></p>
+                <p className="time">09.00 - 11.45 <span className="wib">WIB</span></p>
+              </div>
+            </div>
+          </div>
+          <div className="schedule-item">
+            <div className="schedule-title">
+              <h3>SPECIAL SERVICE WITH PS. PHILIP MANTOFA</h3>
+              <span className="location">UMUM 2</span>
+            </div>
+            <div className="event-details">
+              <p className="trans-convention-center">Trans Convention Center</p>
+              <div className="right-details">
+                <p className="floor">3<sup>FL</sup></p>
+                <p className="time">13.00 - 13.45 <span className="wib">WIB</span></p>
+              </div>
+            </div>
+          </div>
+          <div className="schedule-item">
+            <div className="schedule-title">
+              <h3>SPECIAL SERVICE WITH PS. PHILIP MANTOFA</h3>
+              <span className="location">UMUM 3</span>
+            </div>
+            <div className="event-details">
+              <p className="trans-convention-center">Trans Convention Center</p>
+              <div className="right-details">
+                <p className="floor">3<sup>FL</sup></p>
+                <p className="time">17.00 - 18.45 <span className="wib">WIB</span></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="event-poster">
+        <img
+          src={`${process.env.PUBLIC_URL}/ppm.jpeg`}
+          alt="The End Times Poster"
+        />
+      </div>
     </div>
   );
 }
