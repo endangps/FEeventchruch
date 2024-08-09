7b4147f9f6459d5dbab55711acd5129bcabd840a
endangps <endanglps17@gmail.com>
2024-08-10 00:02:03 +0700

    merapikan

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ function App() {
           <h2>10 AGUSTUS 2024</h2>
         </div>
         <div className="schedule-body">
-          <div className="schedule-item">
+        <div className="schedule-item">
             <div className="schedule-title">
               <h3>YOUTH QUAKE WITH JEREMY MANTOFA</h3>
               <span className="location">AOG 1</span>
             </div>
             <div className="event-details">
-              <p className="mainhall">MAINHALL</p>
+              <p className="Mainhall">Mainhall</p>
               <div className="right-details">
                 <p className="floor">3A<sup>FL</sup></p>
-                <p className="time">15.00 - 16.45 <span className="wib">WIB</span></p>
+                <p className="time">17.00 - 18.45 <span className="wib">WIB</span></p>
               </div>
             </div>
           </div>
@@ -29,7 +29,7 @@ function App() {
               <span className="location">AOG 2</span>
             </div>
             <div className="event-details">
-              <p className="mainhall">MAINHALL</p>
+              <p className="Mainhall">Mainhall</p>
               <div className="right-details">
                 <p className="floor">3A<sup>FL</sup></p>
                 <p className="time">17.00 - 18.45 <span className="wib">WIB</span></p>
@@ -42,10 +42,10 @@ function App() {
               <span className="location">UMUM 1</span>
             </div>
             <div className="event-details">
-              <p className="mainhall">MAINHALL</p>
+              <p className="trans-convention-center">Trans Convention Center</p>
               <div className="right-details">
                 <p className="floor">3<sup>FL</sup></p>
-                <p className="time">09.00 - 11.45 <span className="wib">WIB</span></p>
+                <p className="time">17.00 - 18.45 <span className="wib">WIB</span></p>
               </div>
             </div>
           </div>
